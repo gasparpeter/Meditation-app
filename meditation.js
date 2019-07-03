d61d51cb86c66ebdb3e0510e551b05d556597e2a
Peter Gaspar <petihappygaspar@gmail.com>
2019-07-03 05:55:09 +0300

    - application finished with some errors

--- meditation.js
+++ meditation.js
@@ -1,26 +1,46 @@
 const app = () => {
-    const song = document.querySelector('.song');
-    const play = document.querySelector('.play');
-    const outLine = document.querySelector('.moving-outline circle');
-    const video = document.querySelector('.vid-container video');
-
-
-    const sounds = document.querySelectorAll('.sound-picker button');
-
-    const timeDisplay = document.querySelector('.time-display');
-    const timeSelect = document.querySelectorAll('.time-select button');
-
-    const outLinelength = outLine.getTotalLength();
-
+    const song = document.querySelector(".song");
+    const play = document.querySelector(".play");
+    const replay = document.querySelector(".replay");
+    const outline = document.querySelector(".moving-outline circle");
+    const video = document.querySelector(".vid-container video");
+//Sounds
+    const sounds = document.querySelectorAll(".sound-picker button");
+//Time Display
+    const timeDisplay = document.querySelector(".time-display");
+    const outlineLength = outline.getTotalLength();
+//Duration
+    const timeSelect = document.querySelectorAll(".time-select button");
     let fakeDuration = 600;
 
-    outLine.style.strokeDasharray = outLinelength;
-    outLine.style.strokeDashoffset = outLinelength;
+    outline.style.strokeDashoffset = outlineLength;
+    outline.style.strokeDasharray = outlineLength;
 
 
-    play.addEventListener('click', () =>{
+    sounds.forEach(sound => {
+        sound.addEventListener("click", function() {
+            song.src = this.getAttribute("data-sound");
+            video.src = this.getAttribute("data-video");
+            checkPlaying(song);
+        });
+    });
+
+    play.addEventListener("click", function() {
         checkPlaying(song);
     });
+
+    replay.addEventListener("click", function() {
+        restartSong(song);
+
+    });
+
+
+    const restartSong = song =>{
+        let currentTime = song.currentTime;
+        song.currentTime = 0;
+        console.log("ciao")
+
+    }
 
     timeSelect.forEach(option => {
         option.addEventListener("click", function() {
@@ -35,33 +55,30 @@ const app = () => {
         if (song.paused) {
             song.play();
             video.play();
-            play.src = './svg/pause.svg';
-        }else {
+            play.src = "./svg/pause.svg";
+        } else {
             song.pause();
             video.pause();
-            play.src = './svg/play.svg';
+            play.src = "./svg/play.svg";
         }
+    };
 
-    }
-
-    song.ontimeupdate = () => {
+    song.ontimeupdate = function() {
         let currentTime = song.currentTime;
         let elapsed = fakeDuration - currentTime;
         let seconds = Math.floor(elapsed % 60);
         let minutes = Math.floor(elapsed / 60);
-
-        let progress = outLinelength - (currentTime / fakeDuration) * outLinelength;
-        outLine.style.strokeDashoffset = progress;
-
-
         timeDisplay.textContent = `${minutes}:${seconds}`;
+        let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+        outline.style.strokeDashoffset = progress;
 
         if (currentTime >= fakeDuration) {
-            
+            song.pause();
+            song.currentTime = 0;
+            play.src = "./svg/play.svg";
+            video.pause();
         }
-
-    }
-
+    };
 };
 
 app();
